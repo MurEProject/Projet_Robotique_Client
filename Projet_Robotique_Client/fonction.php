886422ebc2d2ce98f624e6501e05346ba12b4bf0
Pc de laurent <Laurent@MacBook-Pro-de-Pc.local>
2014-06-04 11:40:54 +0200

    maj pour camera

--- Projet_Robotique_Client/fonction.php
+++ Projet_Robotique_Client/fonction.php
@@ -99,15 +99,15 @@
     else if($_POST['id']==14)
 	{
 		$result = socket_read ($socket, 1024) or die("Could not read server response\n");
-        $obj = json_decode($result);
-        $result = $obj->{'Image'};
+        $obj = var_dump(json_decode($result));
+        //$result = $obj->{'Image'};
         //$data = base64_decode($result);
         //$im = imagecreatefromstring($data);
         //header('Content-Type: image/jpeg');
         
         //imagejpeg($im,'imagetest.jpeg');
         
-        echo $result;
+        echo $obj;
 	}
 	else{
 		//$message= serialize($message);
